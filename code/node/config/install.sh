d0a9d94a811daa6d2659624a5c0e0706c735be79
Martin Michalski <martmichals@gmail.com>
2022-11-22 23:00:31 -0600

    Upload of config files for networking

---
+++ code/node/config/install.sh
@@ -0,0 +1,8 @@
+opkg update
+opkg remove wpad-mini
+opkg remove wpad-basic
+opkg remove wpad-basic-wolfssl
+opkg install batctl-full
+opkg install kmod-batman-adv
+opkg install wpad-mesh-wolfssl
+echo "-- DONE INSTALLING PACKAGES --"
