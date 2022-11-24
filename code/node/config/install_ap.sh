27c07ecc64cfad1c2b470547a574c29a3be38e8c
Martin Michalski <martmichals@gmail.com>
2022-11-23 22:46:21 -0600

    Update post script initialization procedure

--- code/node/config/install_ap.sh
+++ code/node/config/install_ap.sh
@@ -36,6 +36,13 @@ wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing
 wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing/post_script.sh
 echo "-- DONE DOWNLOADING TELEMETRY SCRIPTS --"
 
+# Configure the telemetry script to run on startup
+echo "-- CONFIGURING TELEMETRY TO RUN ON START --"
+chmod +x ./post_script.sh
+echo "/root/post_script.sh &" > /etc/rc.local
+echo "exit 0" >> /etc/rc.local
+echo "-- DONE CONFIGURING TELEMETRY TO RUN ON START --"
+
 # Disable unnecessary services
 echo "-- DISABLING SERVICES --"
 service dnsmasq stop && service dnsmasq disable
--- code/node/sensing/post_script.sh
+++ code/node/sensing/post_script.sh
@@ -1,28 +1,42 @@
-# Timestamp
-TIMESTAMP=$(date +%s)
-DATA_FIELDS="timestamp=${TIMESTAMP}"
+#!/bin/sh
 
-# Get Node ID
-ID="node-$(cat /sys/class/net/wlan0/address | cut -d":" -f5- | tr -d :)"
-DATA_FIELDS="${DATA_FIELDS}&id=${ID}"
+# Relative commands
+cd /root
 
-# Get sensor data from telemetry script
-SENSOR_DATA=$(./telemetry)
-if [ ! $? = 0 ]; then
-    echo "Failed to read from sensors."
-    exit
-fi
-DATA_FIELDS="${DATA_FIELDS}&${SENSOR_DATA}"
+# Delay, wait for all interfaces to come online (hopefully)
+sleep 120
 
-# Get mesh connection count 
-NEIGHBORS=$(batctl bat0 n | tail -n +3 | wc -l)
-DATA_FIELDS="${DATA_FIELDS}&meshCount=${NEIGHBORS}"
+while :
+do
+    # 5 second delay
+    sleep 5
 
-# Get client count for AP nodes
-if [ ! "$ID" = "node-a390" ]; then
-    CLIENTS=$(iw dev wlan0-1 station dump | grep -c "^Station")
-    DATA_FIELDS="${DATA_FIELDS}&clientCount=${CLIENTS}"
-fi
+    # Timestamp
+    TIMESTAMP=$(date +%s)
+    DATA_FIELDS="timestamp=${TIMESTAMP}"
 
-# Post the data to the backend with CURL
-curl -XPOST -v "https://charm.twong.dev/telemetry?key=$(cat /root/.key)&${DATA_FIELDS}"
+    # Get Node ID
+    ID="node-$(cat /sys/class/net/wlan0/address | cut -d":" -f5- | tr -d :)"
+    DATA_FIELDS="${DATA_FIELDS}&id=${ID}"
+
+    # Get sensor data from telemetry script
+    SENSOR_DATA=$(./telemetry)
+    if [ ! $? = 0 ]; then
+        echo "Failed to read from sensors." > ./post_debug.log
+        exit
+    fi
+    DATA_FIELDS="${DATA_FIELDS}&${SENSOR_DATA}"
+
+    # Get mesh connection count 
+    NEIGHBORS=$(batctl bat0 n | tail -n +3 | wc -l)
+    DATA_FIELDS="${DATA_FIELDS}&meshCount=${NEIGHBORS}"
+
+    # Get client count for AP nodes
+    if [ ! "$ID" = "node-a390" ]; then
+        CLIENTS=$(iw dev wlan0-1 station dump | grep -c "^Station")
+        DATA_FIELDS="${DATA_FIELDS}&clientCount=${CLIENTS}"
+    fi
+
+    # Post the data to the backend with CURL
+    curl -XPOST -v "https://charm.twong.dev/telemetry?key=$(cat /root/.key)&${DATA_FIELDS}" > ./post_debug.log
+done
