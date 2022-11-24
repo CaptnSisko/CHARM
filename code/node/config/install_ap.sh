ec7e96f54a235dcdf51cbf49336f93061e279870
Martin Michalski <martmichals@gmail.com>
2022-11-23 22:24:10 -0600

    Updated setup scripts

--- code/node/config/install_ap.sh
+++ code/node/config/install_ap.sh
@@ -15,9 +15,11 @@ wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/config/
 wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/config/ap_config/dhcp
 echo "-- DONE PULLING CONFIG FILES --"
 
+# Directory for all the scripts
+cd /root
+
 # Install Onion software packages required for the telemetry script
-echo "-- INSTALLING ONION PACKAGES --"
-cd ~
+echo "-- INSTALLING TELEMETRY PACKAGES --"
 wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/liboniondebug_0.9-1_mipsel_24kc.ipk
 opkg install liboniondebug_0.9-1_mipsel_24kc.ipk
 wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/libonioni2c_0.9-1_mipsel_24kc.ipk
@@ -25,7 +27,14 @@ opkg install libonioni2c_0.9-1_mipsel_24kc.ipk
 rm libonion*
 opkg update
 opkg install curl
-echo "-- DONE INSTALLING ONION PACKAGES --"
+echo "-- DONE TELEMETRY PACKAGES --"
+
+# Download the telemetry script
+echo "-- DOWNLOADING TELEMETRY SCRIPTS --"
+wget https://github.com/CaptnSisko/CHARM/raw/master/code/node/sensing/telemetry
+wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing/adc_params.txt
+wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing/post_script.sh
+echo "-- DONE DOWNLOADING TELEMETRY SCRIPTS --"
 
 # Disable unnecessary services
 echo "-- DISABLING SERVICES --"
@@ -36,8 +45,7 @@ echo "-- DONE DISABLING SERVICES --"
 
 # Reminder to change IP
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "REMINDER: Populate /root/.key"
 echo "REMINDER: Change Node IP"
 echo "REMINDER: Change Mesh Password"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-# TODO: Update links when all config is merged into master
--- code/node/config/install_gateway.sh
+++ code/node/config/install_gateway.sh
@@ -15,9 +15,11 @@ wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/config/
 wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/config/gateway_config/dhcp
 echo "-- DONE PULLING CONFIG FILES --"
 
+# Directory for all the scripts
+cd /root
+
 # Install Onion software packages required for the telemetry script
-echo "-- INSTALLING ONION PACKAGES --"
-cd ~
+echo "-- INSTALLING TELEMETRY PACKAGES --"
 wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/liboniondebug_0.9-1_mipsel_24kc.ipk
 opkg install liboniondebug_0.9-1_mipsel_24kc.ipk
 wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/libonioni2c_0.9-1_mipsel_24kc.ipk
@@ -25,12 +27,19 @@ opkg install libonioni2c_0.9-1_mipsel_24kc.ipk
 rm libonion*
 opkg update
 opkg install curl
-echo "-- DONE INSTALLING ONION PACKAGES --"
+echo "-- DONE TELEMETRY PACKAGES --"
+
+# Download the telemetry script
+echo "-- DOWNLOADING TELEMETRY SCRIPTS --"
+wget https://github.com/CaptnSisko/CHARM/raw/master/code/node/sensing/telemetry
+wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing/adc_params.txt
+wget https://raw.githubusercontent.com/CaptnSisko/CHARM/master/code/node/sensing/post_script.sh
+echo "-- DONE DOWNLOADING TELEMETRY SCRIPTS --"
 
 # Reminder to change mesh password
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "REMINDER: Populate /root/.key"
 echo "REMINDER: Change Mesh Password"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
-# TODO: Update links when all config is merged into master
-# TODO: Update WWAN password, SSID for demo
+# TODO: Update wireless WAN settings for demo. Remember, channels must match
