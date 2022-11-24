c4edf80b7335f8b55746b587859547d86a929389
Martin Michalski <martmichals@gmail.com>
2022-11-23 21:34:56 -0600

    Changing up of installation scripts

--- code/node/config/install_ap.sh
+++ code/node/config/install_ap.sh
@@ -15,6 +15,15 @@ wget https://raw.githubusercontent.com/CaptnSisko/CHARM/telemetry-service/code/n
 wget https://raw.githubusercontent.com/CaptnSisko/CHARM/telemetry-service/code/node/config/ap_config/dhcp
 echo "-- DONE PULLING CONFIG FILES --"
 
+# Install Onion software packages required for the telemetry script
+wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/liboniondebug_0.9-1_mipsel_24kc.ipk
+opkg install liboniondebug_0.9-1_mipsel_24kc.ipk
+wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/libonioni2c_0.9-1_mipsel_24kc.ipk
+opkg install libonioni2c_0.9-1_mipsel_24kc.ipk
+rm libonion*
+opkg update
+opkg install curl
+
 # Disable unnecessary services
 echo "-- DISABLING SERVICES --"
 service dnsmasq stop && service dnsmasq disable
--- code/node/sensing/install_software.sh
+++
@@ -1,8 +0,0 @@
-# Install Onion software packages required for the telemetry script
-wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/liboniondebug_0.9-1_mipsel_24kc.ipk
-opkg install liboniondebug_0.9-1_mipsel_24kc.ipk
-wget http://repo.onioniot.com.s3.amazonaws.com/omega2/packages/onion/libonioni2c_0.9-1_mipsel_24kc.ipk
-opkg install libonioni2c_0.9-1_mipsel_24kc.ipk
-rm libonion*
-opkg update
-opkg install curl
