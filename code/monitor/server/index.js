5e552f7136daa786675b7a2e7fba0bc47be14814
Trevor Wong <trevor8568@gmail.com>
2022-11-29 16:40:27 -0600

    update to support no location

--- code/monitor/server/index.js
+++ code/monitor/server/index.js
@@ -1,33 +1,33 @@
 // Local imports
-const secret = require('./secret').keys
-const log = require('./middleware/log')
-const { corsOptions } = require('./config/corsOptions')
-const { errorHandler } = require('./middleware/errorHandler')
+const secret = require('./secret').keys;
+const log = require('./middleware/log');
+const { corsOptions } = require('./config/corsOptions');
+const { errorHandler } = require('./middleware/errorHandler');
 
 // External imports
-const cors = require('cors')
-const express = require('express')
-const mariadb = require('mariadb')
-const { query } = require('express')
+const cors = require('cors');
+const express = require('express');
+const mariadb = require('mariadb');
+const { query } = require('express');
 
 // Setup application and database
-const app = express()
-const pool = mariadb.createPool(secret.db_config)
+const app = express();
+const pool = mariadb.createPool(secret.db_config);
 
 // Logging
-app.use(log.reqLogger)
+app.use(log.reqLogger);
 
 // Set up middleware
-app.use(cors(corsOptions))
-app.use(express.urlencoded({ extended: false }))
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
 
 // Endpoint to check connection and API key
 app.get('/', (req, res) => {
   if (req.query.key === secret.client.key)
-    res.status(200).json({ message: 'Authentication success. Welcome to the CHARM API.' })
+    res.status(200).json({ message: 'Authentication success. Welcome to the CHARM API.' });
   else
-    res.status(401).json({ error: 'Authentication failure.' })
-})
+    res.status(401).json({ error: 'Authentication failure.' });
+});
 
 // GET all node data
 // FUTURE: Make the database and frontend field names more consistent to make
@@ -46,99 +46,107 @@ app.get('/nodes', async (req, res) => {
         ON mostRecent.id=groupedMostRecent.id
         AND mostRecent.timestamp=groupedMostRecent.maxtime
         `
-      )
+      );
 
       // Update data format
       result = result.reduce(
         (prev, curr) => {
           // Data formatting
-          prev[curr.id] = curr
+          prev[curr.id] = curr;
           prev[curr.id].location = {
             lat: curr.lat,
             lng: curr.lon
-          }
-          prev[curr.id].lastSeen = curr.timestamp
-          delete prev[curr.id].lat
-          delete prev[curr.id].lon
-          delete prev[curr.id].timestamp
+          };
+          prev[curr.id].lastSeen = curr.timestamp;
+          delete prev[curr.id].lat;
+          delete prev[curr.id].lon;
+          delete prev[curr.id].timestamp;
 
           // Filter any null keys
           for (const key in prev[curr.id]) {
             if (prev[curr.id][key] === null)
-              delete prev[curr.id][key]
+              delete prev[curr.id][key];
           }
 
-          return prev
+          return prev;
         }, 
         {}
-      ) 
+      );
 
       // Return the data
-      res.status(200).json(result)
+      res.status(200).json(result);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Failed in querying server database.' });
   }
-})
+});
 
 // POST sensor data
 app.post('/telemetry', async (req, res) => {
   // Check the API key
   if (req.query.key !== secret.client.key) {
-    res.status(401).json({ error: 'Authentication failure.' })
-    return
+    res.status(401).json({ error: 'Authentication failure.' });
+    return;
   }
 
   // Check for id, timestamp, location
-  if (!('id' in req.query && 'timestamp' in req.query && 'lat' in req.query && 'lon' in req.query)) {
-    res.status(400).json({ error: 'Missing id, timestamp, or location.' })
-    return 
+  if (!('id' in req.query && 'timestamp' in req.query)) {
+    res.status(400).json({ error: 'Missing id or timestamp.' });
+    return;
+  }
+
+  if(!('lat' in req.query)) {
+    req.query.lat = null;
+  }
+
+  if(!('lon' in req.query)) {
+    req.query.lon = null;
   }
 
   // Validate numeric fields
-  const time = Number(req.query.timestamp)
+  const time = Number(req.query.timestamp);
   if (!(Number.isInteger(time) && time > 0)) {
-    res.status(400).json({ error: 'Timestamp is not valid.' })
-    return 
+    res.status(400).json({ error: 'Timestamp is not valid.' });
+    return;
   }
   if (isNaN(Number(req.query.lat)) || isNaN(Number(req.query.lon))) {
-    res.status(400).json({ error:  'Lat or lon are not valid numbers.' })
-    return 
+    res.status(400).json({ error:  'Lat or lon are not valid numbers.' });
+    return;
   }
 
   // Validate ID
   if (req.query.id.length <= 9) {
     const validId = await pool.query(`SELECT * FROM nodes WHERE id='${req.query.id}'`);
     if (validId.length !== 1) {
-      res.status(400).json({ error: 'Invalid node ID passed.' })
-      return 
+      res.status(400).json({ error: 'Invalid node ID passed.' });
+      return;
     }
   } else {
-    res.status(400).json({ error: 'ID field is too long.' })
-    return 
+    res.status(400).json({ error: 'ID field is too long.' });
+    return;
   }
 
   // Start query setup
-  const queryColumns = ['id', 'timestamp', 'lat', 'lon']
-  const queryValues = [`'${req.query.id}'`, req.query.timestamp, req.query.lat, req.query.lon]
+  const queryColumns = ['id', 'timestamp', 'lat', 'lon'];
+  const queryValues = [`'${req.query.id}'`, req.query.timestamp, req.query.lat, req.query.lon];
 
   // Push optional arguments
   if ('voltage' in req.query && !isNaN(Number(req.query.voltage))) {
-    queryColumns.push('voltage')
-    queryValues.push(req.query.voltage)
+    queryColumns.push('voltage');
+    queryValues.push(req.query.voltage);
   }
   if ('clientCount' in req.query) {
-    const clientCount = Number(req.query.clientCount)
+    const clientCount = Number(req.query.clientCount);
     if (Number.isInteger(clientCount) && clientCount >= 0) {
-      queryColumns.push('clientCount')
-      queryValues.push(req.query.clientCount)
+      queryColumns.push('clientCount');
+      queryValues.push(req.query.clientCount);
     }
   }
   if ('meshCount' in req.query) {
-    const meshCount = Number(req.query.meshCount)
+    const meshCount = Number(req.query.meshCount);
     if (Number.isInteger(meshCount) && meshCount >= 0) {
-      queryColumns.push('meshCount')
-      queryValues.push(req.query.meshCount)
+      queryColumns.push('meshCount');
+      queryValues.push(req.query.meshCount);
     }
   }
 
@@ -152,20 +160,20 @@ app.post('/telemetry', async (req, res) => {
       `
     );
     if (result.affectedRows !== 1 || result.warningStatus !== 0)
-      throw 'Failure in validation of databased write'
+      throw 'Failure in validation of databased write';
   } catch (err) {
-    res.status(500).json({ error: 'Failed insertion into server database.' })
+    res.status(500).json({ error: 'Failed insertion into server database.' });
   }
 
   // Successful write
-  res.status(200).json({ error: 'Successful query.' })
-})
+  res.status(200).json({ error: 'Successful query.' });
+});
 
 // Error handling and logging
-app.use(errorHandler)
+app.use(errorHandler);
 
 // Start listening for requests
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Test app listening on port ${port}`)
-})
+  console.log(`Test app listening on port ${port}`);
+});
